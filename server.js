5a9da8f7000ca42c98828f64aa59286cf9f05202
mmacari7 <mmacari7@gmail.com>
2019-08-05 14:03:31 -0400

    Boiler Plate

---
+++ server.js
@@ -0,0 +1,14 @@
+// Starts our server
+const express = require("express");
+const app = express();
+
+// Get the base route
+app.get('/', (req, res) => {
+    console.log("Doing nothing");
+    res.json({message: "Hello World"});
+})
+
+
+app.listen(3001, () => {
+    console.log("Server started, listening on port 3001");
+});
