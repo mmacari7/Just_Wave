90c2ebcc2c7bbf574bf8ce653a9b9d64db77c820
mmacari7 <mmacari7@gmail.com>
2019-08-07 16:53:47 -0400

    Use className instead of class in React

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -7,17 +7,17 @@ class Header extends Component {
 
     render() {
         return (
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 class="display-4">Just Wave</h1>
-                    <p class="lead">Welcome to Just Wave, your source for surf and weather information at some of America's hottest surf spots. Select a location for up-to-date reports and to communicate with other local surfers.</p>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Just Wave</h1>
+                    <p className="lead">Welcome to Just Wave, your source for surf and weather information at some of America's hottest surf spots. Select a location for up-to-date reports and to communicate with other local surfers.</p>
                 </div>
                 <nav aria-label="...">
-                    <ul class="pagination pagination-lg justify-content-center">
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item"><a class="page-link" href="#">4</a></li>
+                    <ul className="pagination pagination-lg justify-content-center">
+                        <li className="page-item"><a className="page-link" href="#">1</a></li>
+                        <li className="page-item"><a className="page-link" href="#">2</a></li>
+                        <li className="page-item"><a className="page-link" href="#">3</a></li>
+                        <li className="page-item"><a className="page-link" href="#">4</a></li>
                     </ul>
                 </nav>
             </div>
