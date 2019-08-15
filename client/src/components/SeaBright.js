6ae6ca0f85b3e408a2cfe7171b94a16e8d831111
Michael Macari <mmacari7@gmail.com>
2019-08-14 20:06:39 -0400

    Added all namespaces and individual socket connections

--- client/src/components/SeaBright.js
+++ client/src/components/SeaBright.js
@@ -1,10 +1,19 @@
 import React, {Component} from "react";
 import '../css/index.css'
+import io from 'socket.io-client';
 
+const url = "http://localhost:3000/seabright"
 
 class SeaBright extends Component {
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount() {
+        let socket = io(url);
+        socket.on("connection", () => {
+            console.log("Connected to server");
+        })
     }
 
     render() {
--- server.js
+++ server.js
@@ -15,10 +15,37 @@ app.get('/api/getMe', (req, res) => {
     res.json({data: "Hello World"});
 })
 
+// Defines our socket connections
+// NOTE!!!!::::: COULD CAUSE ISSUES WHEN EXPRESS IS ON LOCALHOST 3000
+const seabright = io.of("/seabright");
+const pipline = io.of("/pipeline");
+const newport = io.of("/newport");
+const laguana = io.of("/laguana");
+const oceancity = io.of("/oceancity")
+
 io.on("connection", (socket) => {
     console.log("Someone connected");
 })
 
+seabright.on("connection", (socket) => {
+    console.log("Someone connected to seabright socket");
+})
+
+pipline.on("connection", (scoket)=> {
+    console.log("Someone connected to pipeline socket");
+})
+
+newport.on("connection", (scoket)=> {
+    console.log("Someone connected to newport socket");
+})
+
+laguana.on("connection", (scoket)=> {
+    console.log("Someone connected to laguana socket");
+})
+
+oceancity.on("connection", (scoket)=> {
+    console.log("Someone connected to ocean city socket");
+})
 
 http.listen(3000, () => {
     console.log("Server started, listening on port 3000");
